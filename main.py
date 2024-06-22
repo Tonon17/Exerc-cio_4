18c47195683f46586ef23b411c43ba09bfbacac2
Tonon17 <tonondouglas3@gmail.com>
2024-06-22 11:40:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+H = float(input("Quanto você ganha por hora? "))
+D = float(input("Quantas horas você trabalha por dia? "))
+M = float(input("Quantos dias você trabalha no mês? "))
+
+S = H * (D * M)
+
+print(f"seu salário mensal é de R${S}0")
